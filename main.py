4faae7f7712e0d2c7bd01320b91f2d7f78f9bf8c
Milan <dangi.milan46@gmail.com>
2025-04-04 11:11:37 +0545

    change

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import db_helper
 import genric_helper
 
 app = FastAPI()
-inprogress_order = {}
+inprogress_orders= {}
 @app.post("/")
 async def handle_request(request:Request):
     
@@ -22,32 +22,67 @@ async def handle_request(request:Request):
     intent_handler_dict = {
         'track.order-context:ongoing-tracking': track_order,
         'order.add-context:ongoing-order' : add_to_order,
-        # 'order.complete-context: ongoing-order': complete_order(),
+        'order.complete-context: ongoing-order': complete_order,
         # 'order.remove-contex:ongoing-order': remove_from_order()  
     }
     return intent_handler_dict[intent](parameters, session_id)
-
-def add_to_order(parameters:dict,session_id:str):
-    food_items = parameters['food-item']
-    item_quantity = parameters['number']
-
-    if len(food_items) != len(item_quantity):
-        fulfillment_text = "Sorry I didn't understand. can you please specify food quantity agian?"
+def save_to_db(order:dict):
+    #taking next order_id 
+    next_order_id = db_helper.get_next_order_id()
+    # adding item to db
+    for item,qty in order.items():
+       rcode =  db_helper.insert_order_item(
+            item,
+            qty,
+            next_order_id
+        )
+       
+       if rcode == -1:
+           return -1
+       
+       db_helper.insert_order_tracking(next_order_id,"in-progress")
+    return next_order_id
+def complete_order(parameters: dict, session_id: str):
+    if session_id not in inprogress_orders:
+        fulfillment_text = "I'm having a trouble finding your order. Sorry! Can you place a new order please?"
     else:
-        new_food_item = dict(zip(food_items,item_quantity))
-        if session_id  in inprogress_order:
-            current_dict = inprogress_order[session_id]
-            for item, qty in new_food_item.items():
-                if item in current_dict:
-                    current_dict[item] +=qty
-                else:
-                    current_dict[item] = qty
-            inprogress_order[session_id]=current_dict
+        order = inprogress_orders[session_id]
+        order_id = save_to_db(order)
+        if order_id == -1:
+            fulfillment_text = "Sorry, I couldn't process your order due to a backend error. " \
+                               "Please place a new order again"
         else:
-            inprogress_order[session_id] = new_food_item
-        order_str = genric_helper.get_str_from_food_dict(inprogress_order[session_id])
+            order_total = db_helper.get_total_order_price(order_id)
 
+            fulfillment_text = f"Awesome. We have placed your order. " \
+                           f"Here is your order id # {order_id}. " \
+                           f"Your order total is {order_total} which you can pay at the time of delivery!"
+
+        del inprogress_orders[session_id]
+
+    return JSONResponse(content={
+        "fulfillmentText": fulfillment_text
+    })
+    
+def add_to_order(parameters: dict, session_id: str):
+    food_items = parameters["food-item"]
+    quantities = parameters["number"]
+
+    if len(food_items) != len(quantities):
+        fulfillment_text = "Sorry I didn't understand. Can you please specify food items and quantities clearly?"
+    else:
+        new_food_dict = dict(zip(food_items, quantities))
+
+        if session_id in inprogress_orders:
+            current_food_dict = inprogress_orders[session_id]
+            current_food_dict.update(new_food_dict)
+            inprogress_orders[session_id] = current_food_dict
+        else:
+            inprogress_orders[session_id] = new_food_dict
+
+        order_str = genric_helper.get_str_from_food_dict(inprogress_orders[session_id])
         fulfillment_text = f"So far you have: {order_str}. Do you need anything else?"
+
     return JSONResponse(content={
         "fulfillmentText": fulfillment_text
     })
@@ -64,34 +99,5 @@ def track_order(parameters:dict):
         "fulfillmentText": fulfillment_text
     })
 
-def complete_order(parameters:dict,session_id:str):
-    if session_id not in inprogress_order:
-        fullfillment_text = "I'm having in trouble finding your order. Sorry! can you place order again"
-    else:
-        order = inprogress_order[session_id]
-        order_id = save_to_db(order)
 
-        if order_id == -1:
-            fullfillment_text = "Sorry, I coudn't process your order due to some errors." \
-                                "Please palce a new order again "
-            
-        else:
-            fullfillment_text = f"Awesome. We have placed your order"\
-                                f"Here is your order id # {order_id}"\
-                                f"your order total is {order_total} and you have to pay when you recieve order"
-def save_to_db(order:dict):
-    #taking next order_id 
-    next_order_id = db_helper.get_next_order_id()
-    # adding item to db
-    for item,qty in order.items():
-       rcode =  db_helper.insert_order_item(
-            item,
-            qty,
-            next_order_id
-        )
-       
-       if rcode == -1:
-           return -1
-       else:
-           order_total = db_helper.get_total_order_price(next_order_id)
-    return next_order_id,order_total
+
